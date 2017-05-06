fe2fab78db767022a59e39cc378516e8b025ede7
human <dip@dip-office.net>
2017-05-06 22:59:02 +0200

    moved Subscription and Listener to Base

--- contracts/Base.sol
+++ contracts/Base.sol
@@ -33,3 +33,38 @@ contract Base {
     function assert(bool expr) { if (!expr) throw; }
 
 }
+
+
+contract SubscriptionBase {
+    enum Status {OFFER, RUNNING, CHARGEABLE, ON_HOLD, CANCELED}
+
+    struct Subscription {
+        address transferFrom;
+        address transferTo;
+        uint pricePerHour;
+        uint nextChargeOn;
+        uint chargePeriod;
+        uint deposit;  //ID in Subscription and VALUE in Offer
+
+        uint startOn;
+        uint validUntil;
+        uint execCounter;
+        bytes descriptor;
+        uint onHoldSince;
+    }
+
+    struct Deposit {
+        uint value;
+        address owner;
+        bytes descriptor;
+    }
+
+}
+
+contract PaymentListener is SubscriptionBase {
+
+    function onPayment(address _from, uint _value, bytes _paymentData) returns (bool);
+    function onSubExecuted(uint subId) returns (bool);
+    function onSubscriptionChange(uint subId, Status status, bytes _paymentData) returns (bool);
+
+}
--- contracts/ExtERC20.sol
+++ contracts/ExtERC20.sol
@@ -15,40 +15,6 @@ import "./ERC20.sol";
 //Ask:
 // Given: subscription one year:
 
-contract SubscriptionBase {
-    enum Status {OFFER, RUNNING, CHARGEABLE, ON_HOLD, CANCELED}
-
-    struct Subscription {
-        address transferFrom;
-        address transferTo;
-        uint pricePerHour;
-        uint nextChargeOn;
-        uint chargePeriod;
-        uint deposit;  //ID in Subscription and VALUE in Offer
-
-        uint startOn;
-        uint validUntil;
-        uint execCounter;
-        bytes descriptor;
-        uint onHoldSince;
-    }
-
-    struct Deposit {
-        uint value;
-        address owner;
-        bytes descriptor;
-    }
-
-}
-
-contract PaymentListener is SubscriptionBase {
-
-    function onPayment(address _from, uint _value, bytes _paymentData) returns (bool);
-    function onSubExecuted(uint subId) returns (bool);
-    function onSubscriptionChange(uint subId, Status status, bytes _paymentData) returns (bool);
-
-}
-
 contract ExtERC20 is ERC20, SubscriptionBase {
     function paymentTo(PaymentListener _to, uint _value, bytes _paymentData) returns (bool success);
     function paymentFrom(address _from, PaymentListener _to, uint _value, bytes _paymentData) returns (bool success);
