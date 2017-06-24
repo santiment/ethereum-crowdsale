b8870aea9a4db679618e1adcb71fab072af2dc01
human <dip@dip-office.net>
2017-06-25 01:05:23 +0200

    [-] cleanup Base contract

--- contracts/Base.sol
+++ contracts/Base.sol
@@ -7,13 +7,12 @@ contract MintableToken {
 
 contract Base {
 
+    function max(uint a, uint b) returns (uint) { return a >= b ? a : b; }
+    function min(uint a, uint b) returns (uint) { return a <= b ? a : b; }
+    function min(uint a, uint b, uint c) returns (uint) { return a <= b ? min(a,c) : min(b,c); }
+
     modifier only(address allowed) {
         if (msg.sender != allowed) throw;
-        _;
-    }
-
-    modifier only2(address allowed1, address allowed2) {
-        if (msg.sender != allowed1 && msg.sender != allowed2) throw;
         _;
     }
 
@@ -26,12 +25,4 @@ contract Base {
         locked = false;
     }
 
-    function max(uint a, uint b) returns (uint) { return a >= b ? a : b; }
-    function min(uint a, uint b) returns (uint) { return a <= b ? a : b; }
-    function min(uint a, uint b, uint c) returns (uint) { return a <= b ? min(a,c) : min(b,c); }
-
-    function assert(bool expr) { if (!expr) throw; }
-
-    event loga(address a);
-    event logs(string s);
 }
