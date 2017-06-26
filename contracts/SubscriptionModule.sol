356a9711395ed55d2311fd5c6491ce1b12062169
human <dip@dip-office.net>
2017-06-26 17:16:42 +0200

    [%] spell check

--- contracts/SubscriptionModule.sol
+++ contracts/SubscriptionModule.sol
@@ -73,7 +73,7 @@ contract PaymentListener {
 contract XRateProvider {
 
     //@dev returns current exchange rate (in form of a simple fraction) from other currency to SAN (f.e. ETH:SAN).
-    //@dev fraction numbers are restricted to uint16 to prevent overflow by calculation;
+    //@dev fraction numbers are restricted to uint16 to prevent overflow in calculations;
     function getRate() returns (uint32 /*nominator*/, uint32 /*denominator*/);
 
     //@dev provides a code for another currency, f.e. "ETH" or "USD"
